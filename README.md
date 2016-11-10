e5855b08d4969256b9f31bdd0e2c567f9ae35fb0
hspille <h.spille@gmail.com>
2016-11-10 12:36:18 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,18 @@
+trump-to-hillary
+=============
+
+
+
+Chrome extension that replaces occurrences of 'trump' with 'hillary'
+
+[Direct download of crx file](https://github.com/hspille/trump-to-hillary/blob/master/CloudToButt.crx?raw=true)
+
+Note that there are forks of this extension that simply replace 'cloud' with 'butt'.
+In my personal opinion, that approach is too broad and it's less funny as a result, but it is clearly a very
+polarizing issue in the cloud-to-butt user community.  Forks are free to do whatever they like.  But officially, _this_ extension replaces only the phrase described above. Thank you for your concern.
+
+Installation
+------------
+
+In Chrome, choose Window > Extensions.  Drag CloudToButt.crx into the page that appears.
+
---
+++ Source/content_script.js
@@ -0,0 +1,53 @@
+function walk(node) 
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+
+
+function handleText(textNode){
+	var textArray = [{
+		originalText: "Donald Trump",
+		updatedText: "Hillary Clinton"
+	},{
+		originalText: "President Trump",
+		updatedText: "President Clinton"
+	}, {
+		originalText: "Trump",
+		updatedText: "Clinton"
+	}
+	];
+
+	for (var enumeration = textArray.length - 1; enumeration >= 0; enumeration--) {
+		// console.log(textArray[enumeration]);
+		var nodeTextToFix = textNode.nodeValue;
+        
+        var huntedPhrase = new RegExp(textArray[enumeration].originalText, "g")
+
+
+		textNode.nodeValue = nodeTextToFix.replace(huntedPhrase, textArray[enumeration].updatedText);
+	};
+}
+walk(document.body);
